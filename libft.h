dd32019376630c06cb721c902ed79440b0214503
Tristan SWART <tswart@c4r16s10.wethinkcode.co.za>
2019-05-28 15:23:05 +0200

    Update

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tswart <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 09:16:23 by tswart            #+#    #+#             */
-/*   Updated: 2019/05/27 13:34:18 by tswart           ###   ########.fr       */
+/*   Updated: 2019/05/28 14:58:32 by tswart           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,5 +71,8 @@ char	*ft_strmap(char const *s, char (*f)(char));
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 int		ft_strequ(char const *s1, char const *s2);
 int		ft_strnequ(char const *s1, char const *s2, size_t n);
+char	*ft_strsub(char const *s, unsigned int start, size_t len);
+char	*ft_itoa(int nb);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
