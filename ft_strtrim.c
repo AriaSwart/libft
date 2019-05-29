5971a3b41bd2ed6fd4edf03bbe5f18f88fd16f15
Tristan SWART <tswart@c4r16s10.wethinkcode.co.za>
2019-05-29 08:41:19 +0200

    Update

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,8 +6,28 @@
 /*   By: tswart <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 12:11:50 by tswart            #+#    #+#             */
-/*   Updated: 2019/05/27 12:23:38 by tswart           ###   ########.fr       */
+/*   Updated: 2019/05/29 08:40:30 by tswart           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+char	*ft_strtrim(char const *s)
+{
+	size_t	i;
+	size_t	n;
+	size_t	t;
+
+	i = 0;
+	if (!s)
+		return (0);
+	t = ft_strlen((char *)s);
+	while (s[i] != '\0' && ft_isspace(s[i]))
+		i++;
+	if (s[i] == '\0')
+		return ("");
+	while (i < t && ft_isspace(s[t - 1]))
+		t--;
+	n = t - i;
+	return (ft_strsub(s, i, n));
+}
