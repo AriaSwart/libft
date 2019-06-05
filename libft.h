ac1df6c359ea364179c871be5cb9d0f74e15b6a3
Tristan SWART <tswart@c4r14s8.wethinkcode.co.za>
2019-06-05 12:14:08 +0200

    Norm

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tswart <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 09:16:23 by tswart            #+#    #+#             */
-/*   Updated: 2019/05/29 10:07:20 by tswart           ###   ########.fr       */
+/*   Updated: 2019/06/05 12:13:57 by tswart           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@
 # include <string.h>
 # include <stdlib.h>
 
-typedef struct	    s_list
+typedef struct		s_list
 {
-    void	    *content;
-    size_t	    content_size;
-    struct s_list   *next;
+    void			*content;
+    size_t			content_size;
+    struct s_list	*next;
 }		    t_list;
 
 void	ft_bzero(void *s, size_t n);
@@ -29,20 +29,20 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
-int	ft_memcmp(const void *s1, const void *s2, size_t n);
-int	ft_isalnum(int c);
-int	ft_isalpha(int c);
-int	ft_isascii(int c);
-int	ft_isblank(int c);
-int	ft_iscntrl(int c);
-int	ft_isdigit(int c);
-int	ft_isgraph(int c);
-int	ft_islower(int c);
-int	ft_isprint(int c);
-int	ft_ispunct(int c);
-int	ft_isspace(int c);
-int	ft_isupper(int c);
-int	ft_isxdigit(int c);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isblank(int c);
+int		ft_iscntrl(int c);
+int		ft_isdigit(int c);
+int		ft_isgraph(int c);
+int		ft_islower(int c);
+int		ft_isprint(int c);
+int		ft_ispunct(int c);
+int		ft_isspace(int c);
+int		ft_isupper(int c);
+int		ft_isxdigit(int c);
 void	ft_putchar(char c);
 void	ft_putstr(char *c);
 void	ft_putnbr(int n);
@@ -52,9 +52,9 @@ void	ft_putstr_fd(char *c, int fd);
 void	ft_putnbr_fd(int n, int fd);
 void	ft_putendl_fd(char *c, int fd);
 size_t	ft_strlen(const char *str);
-int	ft_atoi(char *str);
-int	ft_toupper(int c);
-int	ft_tolower(int c);
+int		ft_atoi(char *str);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 char	*ft_strcpy(char *dst, const char *src);
 char	*ft_strncpy(char *dst, const char *src, size_t n);
 char	*ft_strdup(const char *str1);
@@ -65,8 +65,8 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strstr(const char *haystack, const char *needle);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
-int	ft_strcmp(const char *s1, const char *s2);
-int	ft_strncmp(const char *s1, const char *s2, size_t n1);
+int		ft_strcmp(const char *s1, const char *s2);
+int		ft_strncmp(const char *s1, const char *s2, size_t n1);
 void	*ft_memalloc(size_t size);
 void	ft_memdel(void **ap);
 char	*ft_strnew(size_t size);
@@ -76,8 +76,8 @@ void	ft_striter(char *s, void (*f)(char *));
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 char	*ft_strmap(char const *s, char (*f)(char));
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-int	ft_strequ(char const *s1, char const *s2);
-int	ft_strnequ(char const *s1, char const *s2, size_t n);
+int		ft_strequ(char const *s1, char const *s2);
+int		ft_strnequ(char const *s1, char const *s2, size_t n);
 char	*ft_strsub(char const *s, unsigned int start, size_t len);
 char	*ft_itoa(int nb);
 char	*ft_strjoin(char const *s1, char const *s2);
