78dcd238973c3cdb7d81a4f3a83bef8f1d47d06c
Tristan SWART <tswart@c4r14s8.wethinkcode.co.za>
2019-06-11 11:27:02 +0200

    Update

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: tswart <marvin@42.fr>                      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/05/20 15:17:26 by tswart            #+#    #+#              #
-#    Updated: 2019/05/21 10:41:45 by tswart           ###   ########.fr        #
+#    Updated: 2019/06/11 09:50:02 by tswart           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,9 +16,9 @@ CC = gcc
 
 CFLAGS = -Wall -Werror -Wextra
 
-SOURCES = *.c
+SOURCES = ft_*.c
 
-OBJECTS = *.o
+OBJECTS = ft_*.o
 
 all: $(NAME)
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tswart <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 09:16:23 by tswart            #+#    #+#             */
-/*   Updated: 2019/06/05 13:15:42 by tswart           ###   ########.fr       */
+/*   Updated: 2019/06/11 11:25:58 by tswart           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,5 +86,9 @@ char				*ft_strtrim(char const *s);
 char				**ft_strsplit(char const *s, char c);
 t_list				*ft_lstnew(void const *content, size_t content_size);
 void				ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
+void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
+void				ft_lstadd(t_list **alst, t_list *new);
+void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
+t_list 				*ft_lstmap(t_list *lst, t_list * (*f)(t_list *elem));
 
 #endif
